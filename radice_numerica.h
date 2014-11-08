ded04b53ff0a40739ea0c3fa8b9086c31692ffb6
Leoxur <Leoxur@users.noreply.github.com>
2014-11-08 17:33:21 +0100

    Create radice_numerica.h

---
+++ radice_numerica.h
@@ -0,0 +1,13 @@
+//funzioni e procedure
+int main();
+void inserisci ();
+void inizializza ();
+int radice_numero (int a);
+void stampa_radici ();
+void stampa_prodotto ();
+void regole();
+void controllo ();
+
+//variabili
+int a, b;//Numeri scelti dall'utente per effettuare il prodotto
+int AN, BN, RN, PRD;
